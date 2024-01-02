c819807eb2f4e8554353f6094eb4d40f338c6233
DESKTOP-Q6DCJ27\dpfvm <ryu.coding1@gmail.com>
2024-01-02 19:08:26 +0900

    Feat : Flutter Study_20240102

--- Sample/splash_screen/lib/main.dart
+++ Sample/splash_screen/lib/main.dart
@@ -1,17 +1,19 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(SplashScreen());
+  runApp(const SplashScreen());
 }
 
 class SplashScreen extends StatelessWidget{
+  const SplashScreen({super.key});
+
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
         body: Container(
-          decoration: BoxDecoration(
+          decoration: const BoxDecoration(
             color: Color(0xFFF99231),
           ),
           child: Row(
@@ -20,7 +22,7 @@ class SplashScreen extends StatelessWidget{
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [Image.asset('assets/logo.png',width: 200,),
-                  CircularProgressIndicator(
+                  const CircularProgressIndicator(
                     valueColor: AlwaysStoppedAnimation(
                       Colors.white,
                     ),

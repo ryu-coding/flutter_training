431193df6551d6a22b0428ea95d30bc830c07901
DESKTOP-Q6DCJ27\dpfvm <ryu.coding1@gmail.com>
2024-01-09 17:42:45 +0900

    Feat : Flutter Study_20240109  - ch.15

---
+++ Sample/cf_tube/lib/const/api.dart
@@ -0,0 +1,4 @@
+const API_KEY = 'API Key(GCP)';
+
+const YOUTUBE_API_BASE_URL = 'https://youtube.googleapis.com/youtube/v3/search';
+const CF_CHANNEL_ID = 'UCxZ2AlaT0hOmxzZVbF_j_Sw';
---
+++ Sample/cf_tube/lib/repository/youtube_repository.dart
@@ -0,0 +1,30 @@
+import 'package:cf_tube/const/api.dart';
+import 'package:dio/dio.dart';
+import 'package:cf_tube/model/video_model.dart';
+
+class YoutubeRepository{
+  static Future<List<VideoModel>> getVideos() async{
+    final repo = await Dio().get(
+      YOUTUBE_API_BASE_URL,
+      queryParameters: {
+        'channelId': CF_CHANNEL_ID,
+        'maxResults': 50,
+        'key': API_KEY,
+        'part': 'snippet',
+        'order': 'date',
+      }
+    );
+
+    final listWithData = repo.data['items'].where(
+          (item) =>
+      item?['id']?['videoId'] != null && item?['snippet']?['title'] != null,
+    );
+    return listWithData
+        .map<VideoModel>(
+          (item) => VideoModel(
+        id: item['id']['videoId'],
+        title: item['snippet']['title'],
+      ),
+    ).toList();
+  }
+}
